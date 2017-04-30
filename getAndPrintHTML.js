6eebff8aead6424317f3757d682c1b6cc1df8e99
harishraisinghani <harishraisinghani@gmail.com>
2017-04-29 20:33:22 -0700

    Make GET request and immediately print data to console

---
+++ getAndPrintHTML.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTML() {
+
+    var buffer = "";
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step2.html'
+    };
+
+    https.get(requestOptions, function (response) {
+        response.setEncoding('utf8');
+
+        response.on('data', function (data) {
+            buffer += data;
+        });
+
+        console.log(buffer + '\n');
+    });
+
+}
+
+getAndPrintHTML();
